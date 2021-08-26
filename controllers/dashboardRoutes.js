11b81086685bd30a2f664fe38e1b7a34c49f1126
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-26 18:51:21 -0500

    using authentication for dashboard routes to get user data

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    }
+                },
+            ],
+        });
