e5f1eb9a84ece7d32fe17c408061e87c9f961821
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-26 20:04:54 -0500

    created render function to display the singular item when selected

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -44,3 +44,14 @@ router.get('/post/:id', async (req, res) => {
                 },
             ],
         });
+
+        const post = postData.get({ plain: true });
+
+        res.render('single-post', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
