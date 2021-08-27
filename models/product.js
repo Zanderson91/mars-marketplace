aae6b1b1fce6e61123b0ab9b1cc81edc7d924a01
AngelaTaylor87 <82851741+AngelaTaylor87@users.noreply.github.com>
2021-08-26 19:58:46 -0500

    added product model

--- models/product.js
+++ models/product.js
@@ -1,1 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "Title",
+          },
+        contents: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+          },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            }
+
+        },
+
+    },
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Product;
 
